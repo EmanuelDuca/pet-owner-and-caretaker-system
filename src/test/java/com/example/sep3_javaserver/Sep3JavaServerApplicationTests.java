ad39e7151ffedd30fdc1be97576fbb5da4e5a4c1
MichaelLeo-code <michael.leo.dk@gmail.com>
2023-10-11 10:50:04 +0200

    initial java commit

---
+++ src/test/java/com/example/sep3_javaserver/Sep3JavaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sep3_javaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sep3JavaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
