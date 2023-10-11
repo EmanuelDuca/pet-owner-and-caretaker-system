7b525d3b033730d05c6002e33d942136fb0f3ebd
MichaelLeo-code <michael.leo.dk@gmail.com>
2023-10-11 12:14:33 +0200

    initial Java commit

---
+++ SEP3_JavaServer/SEP3_JavaServer/src/main/java/com/example/sep3_javaserver/Sep3JavaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sep3_javaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep3JavaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sep3JavaServerApplication.class, args);
+	}
+
+}
