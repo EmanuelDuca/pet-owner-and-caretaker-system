f9fdb0d148089e5160c787585ce07bc68e2bfa81
Sevastian-Bahynskyi <baginski.play@gmail.com>
2023-11-27 19:37:18 +0100

    ADD test notification sender

--- SEP3_Tier1/WebAPI/Program.cs
+++ SEP3_Tier1/WebAPI/Program.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using RabbitMQ.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
