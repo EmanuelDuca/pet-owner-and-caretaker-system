4264217d9d7c06ac315d9f0e00874448ad05eaeb
DanTurcanIuliu <turcan.dan.ro@gmail.com>
2023-11-29 17:20:12 +0100

    ServiceProto

--- Tier3Java/src/main/java/dk/via/sep3/DAO/PetServiceDAO.java
+++ Tier3Java/src/main/java/dk/via/sep3/DAO/PetServiceDAO.java
@@ -26,7 +26,7 @@ public class PetServiceDAO implements PetServiceDAOInterface
     @Override
     public PetServiceEntity createService(PetServiceEntity service)
     {
-        service.setStatus(ServiceProto.Status.PLANNED);
+        service.setStatus(ServiceStatus.PLANNED);
         return repository.save(service);
     }
 
