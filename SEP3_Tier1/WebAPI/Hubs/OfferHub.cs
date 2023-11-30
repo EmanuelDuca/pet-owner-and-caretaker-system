ed224e96a221946adf3a20d9197ea1eec57103ae
MichaelLeo-code <michael.leo.dk@gmail.com>
2023-11-30 18:25:26 +0100

    notification started

--- SEP3_Tier1/WebAPI/Hubs/OfferHub.cs
+++ SEP3_Tier1/WebAPI/Hubs/OfferHub.cs
@@ -7,6 +7,7 @@ namespace WebAPI;
 [Authorize]
 public class OfferHub : Hub<IOfferClient>
 {
+    private int count = 0;
     public async Task SendMessage(string user, string message)
     {
         // await Clients.All.Receive("New",$"Thanks, {Context.UserIdentifier.ToString()}, though {Context.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)}");
@@ -14,9 +15,19 @@ public class OfferHub : Hub<IOfferClient>
         await Clients.User("cubeco").Receive("lol", message);
         // await Clients.Client(Context.ConnectionId)
     }
+    
+    public async Task SendOffer()
+    {
+        // await Clients.All.Receive("New",$"Thanks, {Context.UserIdentifier.ToString()}, though {Context.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)}");
+        // await Clients.All.SendAsync("New", user, message);
+        await Clients.All.Notify(++count);
+        // await Clients.Client(Context.ConnectionId)
+    }
 }
 
 public interface IOfferClient
 {
     Task Receive(string user, string message);
+    Task Notify(int numberOfOffers);
+
 }
