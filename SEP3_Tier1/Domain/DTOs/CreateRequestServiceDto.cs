0e31cf24bda4008a135e1d48665ad3f392c81fc7
DanTurcanIuliu <turcan.dan.ro@gmail.com>
2023-11-30 11:44:33 +0100

    Create Service Request Dto

---
+++ SEP3_Tier1/Domain/DTOs/CreateRequestServiceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.DTOs;
+
+public class CreateRequestServiceDto
+{
+    public int InitiatorId { get; set; }
+    public int RecipientId { get; set; }
+    public int Id { get; set; }
+    public int AnnouncementId { get; set; }
+    public int RequestEnum { get; set; }
+    public DateTime CreationDateTime { get; set; }
+}
