1ddd4fd52a82689cb41a96639af5e1ad896b2824
DanTurcanIuliu <turcan.dan.ro@gmail.com>
2023-11-30 10:57:39 +0100

    Service and ServiceRequest Model

---
+++ SEP3_Tier1/Domain/Models/Service.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models;
+
+public class Service
+{
+    public int Id { get; set; }
+    public CareTaker CareTaker { get; set; }
+    public PetOwner PetOwner { get; set; }
+    public Announcement Announcement { get; set; }
+    //private FeedbackEntity feedback { get; set; }
+    public int Status { get; set; }
+}
---
+++ SEP3_Tier1/Domain/Models/ServiceRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models;
+
+public class ServiceRequest
+{
+    public User Initiator { get; set; }
+    public User Recipient { get; set; }
+    public int Id { get; set; }
+    public Announcement Announcement { get; set; }
+    public int status { get; set; }
+    public DateTime CreationDateTime { get; set; }
+}
